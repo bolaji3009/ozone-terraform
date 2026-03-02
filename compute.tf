a5ff876e2a5e54a879dfebda757f1ba8a77820d1
Your Name <you@example.com>
2026-03-02 20:47:58 +0000

    feat: Add integration server and update outputs

--- compute.tf
+++ compute.tf
@@ -41,3 +41,25 @@ resource "google_compute_instance" "hcwathome_server" {
 
   metadata_startup_script = "sudo apt-get update && sudo apt-get install -y python3"
 }
+
+resource "google_compute_instance" "integration_server" {
+  name         = "integration-server"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.self_link
+    subnetwork = google_compute_subnetwork.default.self_link
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install -y python3"
+}
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "openimis_server_ip" {
 output "hcwathome_server_ip" {
   value = google_compute_instance.hcwathome_server.network_interface[0].access_config[0].nat_ip
 }
+
+output "integration_server_ip" {
+  value = google_compute_instance.integration_server.network_interface[0].access_config[0].nat_ip
+}
