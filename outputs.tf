2b34ff1fa61662677d4253ff2a7e004968d31bf4
Your Name <you@example.com>
2026-03-02 20:57:43 +0000

    feat: Add ozone_server and update outputs

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "hcwathome_server_ip" {
 output "integration_server_ip" {
   value = google_compute_instance.integration_server.network_interface[0].access_config[0].nat_ip
 }
+
+output "ozone_server_ip" {
+  value = google_compute_instance.ozone_server.network_interface[0].access_config[0].nat_ip
+}
